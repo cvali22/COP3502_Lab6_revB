bced7f9f8ca2c1e09052d510e7bdf42432f77034
Andres D <148909842+adelaflor-uf@users.noreply.github.com>
2023-10-25 10:26:01 -0400

    Added decode function and made it so the program works with it

--- lab6.py
+++ lab6.py
@@ -2,7 +2,21 @@
 #Corey Cavalli
 #Lab 6
 
+
+# Implemented by Andres De la Flor
+# Decodes the encrypted password
+def decode(password):
+    original = ""
+
+    # Loop through the password to shift each character back
+    for digit in password:
+        shifted = (int(digit) + 10 - 3) % 10 # We add 10 so that we can shift back by 3 without going to negatives
+        original += str(shifted)
+
+    return original
+
 if __name__ == '__main__':
+    en_password = "" # Declaring en_password here so it is accessible everywhere
 
     option = 0
     while(option != 3): #Prompt user with menu until they select quit (option 3)
@@ -10,7 +24,7 @@ if __name__ == '__main__':
         option = int(input("Please enter an option: ")) #get user selection
 
         if(option == 1): #encode
-            en_password = '' #new string that holds the encoded password
+            # en_password = '' #new string that holds the encoded password
             password = input("Please enter your password to encode: ")
 
             for index in range(len(password)): #perform the encryption by shifting each number by 3
@@ -28,5 +42,4 @@ if __name__ == '__main__':
             print("Your password has been encoded and stored!\n")
 
         elif(option == 2): #decode
-            pass
-#------------------------- LAB PARTNER IMPLEMENT DECODE HERE----------------------------------------
+            print(f"The encoded password is {en_password}, and the original password is {decode(en_password)}.")
